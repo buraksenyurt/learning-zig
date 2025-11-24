0e6a156b69d6d0c37dac55b5cca671e2d2820050
buraksenyurt <burakselimsenyurt@gmail.com>
2025-11-24 22:12:22 +0300

    Enhance pointer examples with multi-item pointer operations and dereferencing

---
+++ 10_pointers.zig
@@ -0,0 +1,59 @@
+const std = @import("std");
+
+// Value referans etmek için &, değere ulaşmak için (dereference) * operatörü kullanılır.
+// C dile ile uyumluluğu sağlamak adına Single-Item ve Multi-Item pointers gibi kavramlar olduğunu söyleyebiliriz.
+pub fn main() void {
+    var number: i32 = 23;
+    std.debug.print("Number value : {d}\n", .{number});
+    addOne(&number); // burada number değişkeninin referansı addOne fonksiyonuna taşınıyor
+    std.debug.print("After add, {d}\n", .{number});
+
+    // pointer'ları const ile de tanımlayabiliriz
+    const luckySeven: u8 = 7;
+    std.debug.print("Lucky Seven : {d}\n", .{luckySeven});
+
+    // // const olarak tanımlanan pointer değerleri kesin olarak değiştirilemez.
+    // // Aşağıdaki kod parçasında iki hata söz konusudur
+    // var newValue = &luckySeven; // error: local variable is never mutated
+    // newValue.* += 1; // error: cannot assign to constant
+
+    // Bu bilgi şu anda ne kadar önemli bilemedim ama
+    // usize ve isize pointer'lar ile aynı boyuta sahiplermiş
+    std.debug.print("Size of usize->{d} and size of *u8->{d}\n", .{ @sizeOf(usize), @sizeOf(*u8) });
+    std.debug.print("Size of isize->{d} and size of *u8->{d}\n", .{ @sizeOf(isize), @sizeOf(*u8) });
+
+    // pointer'lar daha çok derleme zamanında ne kadar yer kaplayacakları
+    // bilinmeyen buffer alanların ele alınmasında işe yarar.
+    // Bunun için Many-Item pointers kavramı kullanılıyor.
+    // Hatta single-item pointer ile bir kıyaslama da var.
+    // Detaylar için https://zig.guide/language-basics/many-item-pointers
+    // Many-Item pointer'lar *T yerine [*]T olarak ifade edilir.
+    // Aşağıdaki örneğe inceleyelim
+    var numbers = [_]u8{ 10, 20, 30 };
+    var ptrNumbers: [*]u8 = &numbers; // multi-item pointer
+    std.debug.print("\nType of the ptrNumbers is {}\n", .{@TypeOf(ptrNumbers)});
+    ptrNumbers[0] = 11;
+    std.debug.print("\nptrNumbers[0]={}, numbers[0]={}", .{ ptrNumbers[0], numbers[0] });
+    // Aşağıdaki satırda pointer 2 artırılıyor.
+    // Bu aslında 0ncı konumda olan pointer'ın bir u8 kadar ötelenmesi olarak düşünülebilir.
+    // Buna göre ptrNumbers[0] artık numbers'ın 30 numaralı 3ncü indisteki değerini işaret edecekken,
+    // numbers[0] halen daha 1nci indisteki 11 olarak değişmiş değeri taşıyacaktır.
+    ptrNumbers += 2;
+    std.debug.print("\nptrNumbers[0]={}, numbers[0]={}", .{ ptrNumbers[0], numbers[0] });
+
+    // Şimdi tekrar -2 çıkartırsak 0ncı indis konumuna dönebiliriz.
+    ptrNumbers -= 2;
+    std.debug.print("\nptrNumbers[0]={}, numbers[0]={}", .{ ptrNumbers[0], numbers[0] });
+
+    // Peki aralık dışı bir değere set edersek ne olacak?
+    ptrNumbers += 5;
+    // Çalışma zamanında pointer'ın 5 ileri götürülmesi sonrası her seferinde değişen u8 değerleri elde edilir
+    // Bu iyi bir şey mi emin olamadım.
+    std.debug.print("\nptrNumbers[0]={}, numbers[0]={}", .{ ptrNumbers[0], numbers[0] });
+}
+
+// addOne parametre olarak i32 türünden bir pointer almakta
+// Örneğe göre &number, değişken pointer buraya atanıyor
+fn addOne(n: *i32) void {
+    n.* += 1; // burada dereference edilen değer 1 artırılıyor
+}
--- README.md
+++ README.md
@@ -46,7 +46,7 @@ zig test tests.zig
 - Yorum satırları **//** ile yazılıyor ve birçok dilin aksine **multi-line comments** bulunmuyor. Yani `/* */` şeklinde bir kullanım yok. Rust dilindekine benzer şekilde **//!** ve **///** ile kod dokümantasyonu sağlanabiliyor.
 - Executable'ların mutlaka bir **main** fonksiyonu içermesi gerekiyor.
 - Rust dilindekine benzer primitive tipler söz konusu. **u8, i8, u16, i16, u32, i32, u64, i64, f32 ve f64** gibi. Ancak bunların haricinde **u47, i47, f16, f80 ve f128** gibi farklı sayısal türler de mevcut. Aslında u47 ve i47 hemen dikkat çekiyor hatta enum türlerinde u2,u5 gibi tag type'lar söz konusu. Bunu keyfi bit uzunlukları *(abitrary bit-width)* ile tanımlanan türler olarak düşünebiliriz. Burada da Rust dilinde olduğu gibi **usize** türü var.
-- Ama **String** diye bir tür yok. **String** aslında **u8** türünden bir dizi olarak ele alınıyor. Örneğin **fullName** isimli bir **struct** alanı, **fullName : []const u8** şeklinde tanımlanıyor.
+- Ama **String** diye bir tür yok. **String** aslında **u8** türünden bir dizi veya slice olarak ele alınıyor. Örneğin **fullName** isimli bir metinsel bir **struct** alanı, **fullName : []const u8** şeklinde tanımlanıyor.
 - Fonksiyon parametreleri **constant** olarak geçiyor. Dolayısıyla metot içerisinde parametre değerleri değiştirilemiyor *(İspat aranacak)*
 - Fonksiyonların aşırı yüklenmesi *(Function Overloading)* gibi bir durum söz konusu değil.
 - Farklı bir modül yazmak oldukça basit. Yeni bir dosya oluşturup içerisine fonksiyon, sabit, yapı vs. tanımlayıp başka bir dosyada **@import("dosya_adı.zig")** ile kullanmak yeterli.
