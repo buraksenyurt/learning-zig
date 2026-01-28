0b8b64f99cc42b88bab0c2ceb86b239136325e63
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-01-29 00:44:01 +0300

    Add JSON serialization example for Game struct and create game_info.json

---
+++ 53_json_serde.zig
@@ -0,0 +1,90 @@
+// Zig programlamada verileri JSON formatında serileştirmek
+// için std.json modülünü kullanabiliriz.
+const std = @import("std");
+const json = @import("std").json;
+
+// Öncek örnek bir struct tanımlayalım.
+// Burada sembolik olarak oyun bilgilerini tutuyoruz.
+const Game = struct {
+    title: []const u8,
+    genre: []const u8,
+    releaseYear: u16,
+    onSale: bool,
+};
+
+pub fn main() !void {
+    // Serileştirme işlemlerinde Allocator'lar kullanarak heap'de alanlar açmamız gerekiyor.
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const pacManGame = Game{
+        .title = "Pac Man",
+        .genre = "Arcade",
+        .releaseYear = 1980,
+        .onSale = true,
+    };
+
+    // JSON'a serileştirme kısmı. Burada u8 türünden bir ArrayList kullandık.
+    // ArrayList dinamik olarak büyüyebilen bir dizi yapısı gibi düşünülebilir
+    // Bu nedenle heap'te yer açmak için allocator kullanıyoruz.
+    var buffer = std.ArrayList(u8).init(allocator);
+    defer buffer.deinit(); // Çıkarken kaynakları serbest bırak
+
+    // JSON Serileştirme işinin yapıldığı yer burası
+    // İlk parametre serileştirilecek değişken,
+    // İkinci parametre bazı opsiyonel ayarlar. Örneğin girinti değeri 3 karaketer
+    // Son parametre de serileştirilmiş içeriği yazacağımız buffer ki bu da allocator
+    // yoluyla heap'te açılmış bir ArrayList.
+    try json.stringify(
+        pacManGame,
+        .{ .whitespace = .indent_3 },
+        buffer.writer(),
+    );
+    // Buffer içeriğini ekrana yazdırıp bir görelim
+    std.debug.print("Serialized JSON:\n{s}\n", .{buffer.items});
+
+    // Burada da içeriği bir dosyaya yazmaya çalışıyoruz.
+    const filePath = "game_info.json";
+    var file = try std.fs.cwd().createFile(
+        filePath,
+        .{ .truncate = true }, // Her seferinde dosyayı sıfırlıyoruz
+    );
+    defer file.close();
+    // buffer içeriğini dosyaya writeAll fonksiyonu ile aktardık
+    try file.writeAll(buffer.items);
+    std.debug.print("JSON data written to {s}\n", .{filePath});
+
+    // Şimdi de deneysel çalışmaya dosyadan içeriği okuma adımı ile devam ediyoruz
+    const readFile = try std.fs.cwd().openFile(filePath, .{});
+    defer readFile.close();
+
+    // Dosta içeriğini okurken maksimum 1 MB lık bir buffer kullandık
+    // ki bizim örnek için epey fazla. Daha büyük boyutlar için nasıl bir ayarlama yapılır
+    // incelemek lazım.
+    const jsonData = try readFile.readToEndAlloc(
+        allocator,
+        1024 * 1024,
+    );
+    defer allocator.free(jsonData);
+
+    // Şimdi de dosyadan okunan içeriği parseFromSlice metodu yardımıyla
+    // Game türüne dönüştürüyoruz.
+    // İlk parametre dönüştürülecek veri yapısı, ikinci parametre allocator nesnemiz,
+    // üçüncü parametre JSON verisi, dördüncü parametre ise opsiyonel ayarlar yer alıyor.
+    const parsedJson = try json.parseFromSlice(
+        Game,
+        allocator,
+        jsonData,
+        .{},
+    );
+    defer parsedJson.deinit();
+
+    std.debug.print("\nDeserialized Game Data:\n", .{});
+    std.debug.print("Title: {s}\nGenre: {s}\nRelease Year: {d}\nOn Sale: {}\n", .{
+        parsedJson.value.title,
+        parsedJson.value.genre,
+        parsedJson.value.releaseYear,
+        parsedJson.value.onSale,
+    });
+}
