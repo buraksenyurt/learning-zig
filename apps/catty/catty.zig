6a5ed23eec35e66a116a778102df573af47bcb1b
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-01-29 14:51:01 +0300

    Add pipeline read support via stdin

--- apps/catty/catty.zig
+++ apps/catty/catty.zig
@@ -1,16 +1,12 @@
-// cat uygulamasının çok basit bir klonu.
-// ve genel amaç fixed-size buffer kullanmak, IO hatalarını ele almak.
-// cat uygulaması felsefe olarak bir veya daha fazla byte'tan oluşan stream'leri tek bir byte stream içerisine almayı hedefler.
-// Verinin text, binary olması veya network hattından gelmesi ya da bir sürücü çıktısı (device output) cat uygulaması için çok
-// önemli değildir. Zira Unix felsefesi şunu öğütler; Write programs that do one thing well, Write programs to work together
-// Programı doğrudan zig dosyası ile çalıştırmak için aşağıdaki komutu kullandım(Windows 11 OS üzerinde)
-// zig run catty.zig -- ./samples/file_1.txt ./samples/file_2.txt ./samples/games.json ./samples/games.dat
-
+// CLONE   : Unix Cat programının hafifsiklet bir sürümü
+// AMAÇLAR : Allocator kullanımını kavramak, IO hatalarını ele almak, komut satırından arguman okumak, anytype kullanmak
+// YORUM   : Cat uygulaması felsefe olarak bir veya daha fazla byte'tan oluşan stream'leri tek bir byte stream içerisine almayı hedefler.
+//           Verinin text, binary olması veya network hattından gelmesi ya da bir sürücü çıktısı (device output) cat uygulaması için çok
+//           önemli değildir. Zira Unix felsefesi şunu öğütler; Write programs that do one thing well, Write programs to work together
 const std = @import("std");
 const fs = std.fs;
 
 pub fn main() !void {
-    // Adım 1: Komut satırından gelen argümanların okunması
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
@@ -18,14 +14,11 @@ pub fn main() !void {
 
     const arguments = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, arguments);
+    const stdout = std.io.getStdOut().writer();
 
     if (arguments.len < 2) {
-        std.log.err("Argument error", .{});
-        std.debug.print("Usage: cat <file1> [file2...]", .{});
-        return;
+        try catStdin(stdout);
     }
-    std.debug.print("\n", .{});
-    const stdout = std.io.getStdOut().writer();
 
     var i: usize = 1;
     while (i < arguments.len) : (i += 1) {
@@ -33,6 +26,7 @@ pub fn main() !void {
     }
 }
 
+// zig run catty.zig -- ./samples/file_1.txt ./samples/file_2.txt ./samples/games.json ./samples/games.dat
 fn catSingleFile(path: []const u8, stdout: anytype) !void {
     var file = try fs.cwd().openFile(path, .{});
     defer file.close();
@@ -45,5 +39,17 @@ fn catSingleFile(path: []const u8, stdout: anytype) !void {
 
         try stdout.writeAll(buffer[0..n]);
         try stdout.print("\n", .{});
+    }
+}
+
+// echo "Hello World from pipeline" | zig run .\catty.zig
+fn catStdin(stdout: anytype) !void {
+    var stdin = std.io.getStdIn().reader();
+    var buffer: [8 * 1024]u8 = undefined;
+
+    while (true) {
+        const n = try stdin.read(&buffer);
+        if (n == 0) break;
+        try stdout.writeAll(buffer[0..n]);
     }
 }
