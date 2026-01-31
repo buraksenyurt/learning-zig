7e1e18bceaf0fdb8dc99c346089c66be5c20211b
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-02-01 00:18:18 +0300

    Enhance zgrep functionality with case-insensitive search and improved argument handling

--- apps/zgrep/zgrep.zig
+++ apps/zgrep/zgrep.zig
@@ -3,8 +3,9 @@
 //          - Allocator kullanımı,
 //          - Komut satırından arguman okuma,
 //          - Dosya okuma işlemi
-// RUN      :
-// YORUM    : zig run .\zgrep.zig -- Sample .\test-file.txt
+// RUN      : zig run .\zgrep.zig -- Sample .\test-file.txt
+//            zig run .\zgrep.zig -- -ics sAMplE .\test-file.txt
+// YORUM    :
 
 const std = @import("std");
 
@@ -19,14 +20,35 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args); // Programdan çıkarken argümanları temizle
 
-    // Hafif bir kontrol. Beklentimiz en az 3 argüman olması
-    if (args.len < 3) {
-        try printUsage(); // Eğer değilse küçük bir kullanım kılavuzu gösterelim
+    var query: []u8 = "";
+    var filePath: []u8 = "";
+    var ignoreCase = false;
+
+    // Argümanları dolaşalım.
+    // İlk index uygulamayı işaret ettiğinden 1den başlıyoruz
+    var index: usize = 1;
+    while (index < args.len) : (index += 1) {
+        // komut satırı argümanını alalım
+        // Argümanları sıralı olarak değerlendirebiliriz, o nedenle sıradaki argümanı kontrol edip
+        // doğru değişkenlere bağlıyoruz.
+        const arg = args[index];
+        // eğer -ics yazıyorsa ignore case sensitive anlamındadır
+        if (std.mem.eql(u8, arg, "-ics")) {
+            ignoreCase = true;
+        } else if (query.len == 0) {
+            query = arg;
+        } else if (filePath.len == 0) {
+            filePath = arg;
+        }
     }
 
-    // Argümaları değişkenlere bağlayalım (bind)
-    const query = args[1];
-    const filePath = args[2];
+    // Döngü, argüman sayısı kadar devam ettiği için
+    // sorgu ifadesi ve dosya bilgilerinin verilip verilmediğine bakıp
+    // kullanım kılavuzunu yayınlıyoruz.
+    if (query.len == 0 or filePath.len == 0) {
+        try printUsage();
+        return;
+    }
 
     // İkinci parametre ile belirtilen dosyayı okuma amaçlı açıyoruz
     var file = try std.fs.cwd().openFile(filePath, .{});
@@ -37,24 +59,50 @@ pub fn main() !void {
     var stream = buffReader.reader();
     var buffer: [4096]u8 = undefined; // 4 KB'lık bloklar halinde okuma yapacağız
 
+    var lineNumber: usize = 0;
     // Dosya sonuna gelinceye kadar okuma yapacak bir while döngüsü
     // Windows, Linux, Unix fark eder mi?
+
     while (try stream.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-        // mem modülündeki indexOf fonksiyonu ile aranan query'nin satır içerisinde
-        // geçip geçmediğini kontrol ediyoruz
-        if (std.mem.indexOf(u8, line, query) != null) {
-            // Eğer geçiyorsa ekrana yazdırıyoruz
-            try std.io.getStdOut().writer().print("{s}\n", .{line});
+        // -ics (is case sensitive) flag aktifse
+        if (ignoreCase) {
+            // yardımcı fonksiyon üzerinden arama yaptırılıyor
+            if (containsCaseInsensitve(line, query)) {
+                try std.io.getStdOut().writer().print("{d}: {s}\n", .{ lineNumber, line });
+            }
+        } else {
+            // aksi durumda Case Sensitive arama söz konusu
+            if (std.mem.indexOf(u8, line, query) != null) {
+                try std.io.getStdOut().writer().print("{d}: {s}\n", .{ lineNumber, line });
+            }
         }
+        lineNumber += 1;
     }
 }
 
 fn printUsage() !void {
     try std.io.getStdOut().writer().print(
-        \\zgrep : A grep clone written with zig programming language
+        \\zgrep : A grep clone written in Zig
         \\
         \\Usages;
-        \\zgrep <query> <file_path>
+        \\zgrep [-ics] <query> <file_path>
+        \\Arguments:
+        \\  -ics    : Ignore case sensitivity while searching (optional)
         \\
     , .{});
+}
+
+// Bu yardımcı fonksiyon küçük büyük harf duyarlılığı geçersiz olduğunda devreye giriyor
+fn containsCaseInsensitve(content: []const u8, searchTerm: []const u8) bool {
+    if (content.len == 0) return true;
+    if (searchTerm.len > content.len) return false;
+
+    var i: usize = 0;
+    while (i <= content.len - searchTerm.len) : (i += 1) {
+        const part = content[i .. i + searchTerm.len];
+        if (std.ascii.eqlIgnoreCase(part, searchTerm)) {
+            return true;
+        }
+    }
+    return false;
 }
