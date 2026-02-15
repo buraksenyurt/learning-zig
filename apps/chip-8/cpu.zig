1fc2e58bc9894273d2820a61eaa4f35b3e8599b0
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-02-15 20:11:26 +0300

    Implement REPL function and ROM loading for CHIP-8 emulator

--- apps/chip-8/cpu.zig
+++ apps/chip-8/cpu.zig
@@ -27,4 +27,89 @@ pub const Cpu = struct {
             .key = [_]u8{0} ** 16,
         };
     }
+    pub fn repl(self: *@This()) void {
+        // Bu fonksiyon instruction yani operasyon kodunu alan FETCH,
+        // Bu operasyon kodunun ne anlama geldiğini çözem DECODE,
+        // Ve çözülen operasyon koduna göre bir takım matematik işlemleri yapan ya da bir şeyler çizen
+        // veya bellekte bir yerlere sıçramayı sağlayan EXECUTE aşamalarını ele alır.
+
+        // Bellekten 2 byte'lık komutu oku. CHIP-8'de her komut 2 byte uzunluğunda ama bellek 1 byte genişliğinde.
+        // Bu nedenle, iki byte'ı birleştirerek tek bir 16-bit opcode oluşturulmakta.
+        const operation_code: u16 = (@as(u16, self.memory[self.pc]) << 8) | self.memory[self.pc + 1];
+        self.pc += 2; // Her komut 2 byte olduğu için program counter'ı 2 artırıyoruz
+
+        // Burada bitwise maskeleme işlemi yapılır ve böylece Operasyon kodu parçalara bölünebilir.
+        // Operasyon kodları önemli zira herbiri bir işlevi temsil etmekte.
+        // https://chip8.gulrak.net/ adresinde detaylı bir içerik de mevcut.
+        switch (operation_code & 0xF000) {
+            0x0000 => {
+                // 00E0 (Clear Screen) anlamında
+                // 00EE ise (Return) anlamında
+                switch (operation_code & 0x00FF) {
+                    0xE0 => {
+                        // Clear Screen komutu söz konusu ise @memset fonksiyonu ile gfx
+                        // alanının tüm elemanları sıfıra çekilir. Yani ekran karartılır.
+                        @memset(&self.gfx, 0);
+                    },
+                    0xEE => {
+                        // Return ile bir önceki stack noktasına dönmemiz gerekiyor
+                        // O nedenle sp değeri 1 azaltılır ve pc isimli stack'te bu indisli yere gidilir.
+                        self.sp -= 1;
+                        self.pc = self.stack[self.sp];
+                    },
+                    else => {},
+                }
+            },
+            0x1000 => {
+                // 1NNN: NNN adresine sıçrama(JUMP) komutu gelmiştir
+                // Bu durumda operasyon kodundan adres kısmını alıp (0x0FFF değeri ile bit maskeleme),
+                // güncel komut adresi değiştirilir
+                self.pc = operation_code & 0x0FFF;
+            },
+            0x6000 => {
+                // 6XNN: VX register'ını NN'e set et der.
+                // Operasyon Kodu şöyle bir anlama geliyor: 6 (Set), X (Reg Index), N N (Value)
+                const x = (operation_code & 0x0F00) >> 8; // 0 ile 15 arasındaki index değerini elde etmek için 8 bit sağa kaydırma
+                const nn = @as(u8, @truncate(operation_code & 0x00FF));
+                self.v[x] = nn;
+            },
+            0x7000 => {
+                // 7XNN komutu söz konusu ise NN değeri VX register'ına eklenir
+                const x = (operation_code & 0x0F00) >> 8;
+                // @truncate ile 16 bit'lik operation_code'un sadece son 8 bit'ini alarak NN değerini elde ediyoruz.
+                const nn = @as(u8, @truncate(operation_code & 0x00FF));
+                //
+                self.v[x] +%= nn;
+            },
+            0xA000 => {
+                // ANNN: Index Register(I) bilgisi NNN değeri ile günceller
+                // Operasyon kodundan adres kısmını alıp (0x0FFF değeri ile bit maskeleme) yapılarak bu işlem gerçekleştirilir.
+                self.i = operation_code & 0x0FFF;
+            },
+            // Burada daha birçok operasyon komutu söz konusu. Bunları şimdilik konu dışı bırakalım
+            else => {
+                std.debug.print("Unknown opcode: {X:0>4}\n", .{operation_code});
+            },
+        }
+
+        // CHIP-8'de delay timer ve sound timer 60 Hz hızında çalışır.
+        // Yani her saniyede 60 kez bu timer'ların değeri 1 azalır.
+        if (self.delay_timer > 0) self.delay_timer -= 1;
+        if (self.sound_timer > 0) self.sound_timer -= 1;
+    }
+
+    // Örneğin bir oyunu ROM'a yüklemek istersek bu fonksiyon yardımıyla bunu yapabiliriz.
+    // ROM dosyasını açar, içeriğini okur ve bellekte 0x200 adresinden itibaren yerleştirir.
+    // 0x200 adresi, CHIP-8'de programların başladığı standart adrestir.
+    // İlk 512 byte'lık alan genellikle sistem tarafından rezerve edilir.
+    pub fn loadRom(self: *@This(), fileName: []const u8) !void {
+        const file = try std.fs.cwd().openFile(fileName, .{});
+        defer file.close();
+
+        const size = try file.readAll(self.memory[0x200..]);
+        std.debug.print("ROM load is OK! {s} ({d} bytes)\n", .{
+            fileName,
+            size,
+        });
+    }
 };
--- apps/chip-8/main.zig
+++ apps/chip-8/main.zig
@@ -1,6 +1,7 @@
-// Bu örnekte Joseph Weisbecker tarafından geliştirilmiş CHIP-8 isimli interpretter'ın bir uygulaması ele alınmaktadır.
-// CHIP-8 hakkında detaylı bilgi almak için https://tr.wikipedia.org/wiki/CHIP-8
-// Amaç hem zig pratiği yapmak hem de bilgisayarın gerçekten nasıl çalıştığını anlamak. CHIP-8'in temel özellikleri şunlardır:
+// Joseph Weisbecker tarafından geliştirilmiş CHIP-8 isimli yorumlayıcının zig ile temel bir klonu
+// CHIP-8 hakkında detaylı bilgi almak için https://chip-8.github.io/links/
+// Amaç hem zig pratiği yapmak hem de bilgisayarın gerçekten nasıl çalıştığını anlamak.
+// CHIP-8 emülatörünün temel muhteviyatı:
 // - 4K bellek (4096 byte)
 // - 16 adet 8-bit register (V0-VF)
 // - Stack. Çağırılan fonksiyonların geri dönüş adreslerini tutar.
@@ -8,10 +9,34 @@
 // - Input. 16 tuşlu hex klavye (0-9, A-F)
 
 const std = @import("std");
-const cpu = @import("cpu.zig").Cpu;
+const Cpu = @import("cpu.zig").Cpu;
 
-pub fn main() void {
+pub fn main() !void {
     std.debug.print("CHIP-8 Emulator", .{});
-    const the_cpu = cpu.init();
-    _ = the_cpu;
+    // CHIP-8 Rom dosyaları için https://github.com/loktar00/chip8/tree/master/roms adresine epey örnek var.
+    var cpu = Cpu.init();
+    // IBM_Logo.ch8 isimli rom dosyası, CHIP-8'de ekranın ortasında IBM logosunu çizen bir program içerir.
+    try cpu.loadRom("ibm_logo.ch8");
+
+    // Emülatörün ana döngüsü.
+    // Bu döngüde CPU'nun repl fonksiyonu çağrılır ve böylece CHIP-8 programı çalıştırılır.
+    while (true) {
+        cpu.repl();
+
+        // CPU'nun program counter'ı her 10 komutta bir ekrana yazdırılır.
+        // Böylece programın nasıl ilerlediğini gözlemleyebiliriz.
+        // Normalde gerçek bir emülatör yazmadığımızdan şimdilik bu kadar basit bir çıktı yeterli olur.
+
+        if (cpu.pc % 10 == 0) {
+            std.debug.print("PC: {x} I:{x} V0: {d}\n", .{
+                cpu.pc,
+                cpu.i,
+                cpu.v[0],
+            });
+        }
+
+        std.time.sleep(2 * 1000 * 1000);
+        // 2 ms bekle. CHIP-8'de her komut yaklaşık 500 Hz hızında çalışmakta.
+        // Bir başka deyişle her 2 mili saniyede bir komut çalıştırılır.
+    }
 }
